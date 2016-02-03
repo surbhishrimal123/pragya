43c240ad5a1df75404a6fa46671ccc1b683b3511
surbhishrimal123 <er.pragya1995@gmail.com>
2016-02-03 16:08:16 +0530

    Update App.java
    added new lcode

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -11,3 +11,10 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+    }
+}
