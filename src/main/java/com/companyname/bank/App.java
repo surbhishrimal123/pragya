9bf849251f576e3a5d6ff094b20ab67b7ad7c6eb
surbhishrimal123 <er.pragya1995@gmail.com>
2016-02-03 16:01:43 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -11,3 +11,10 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+public class app1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hello I am Pragya!" );
+    }
+}
